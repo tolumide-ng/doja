5db052d5b8864882734cdeac704a81643b419e45
Tolumide Shopein <tolumideshopein@gmail.com>
2024-09-18 01:22:24 +0200

    Initial creation of the accumulator using AVX2

--- src/nnue_/accumulator.rs
+++ src/nnue_/accumulator.rs
@@ -1,66 +1,30 @@
 // when the king moves, the accumulator is refreshed
 
-use crate::color::Color;
+use std::arch::x86_64::__m256i;
+
+use crate::{color::Color, constants::PLAYERS_COUNT};
 
 use super::{feature_idx::FeatureIdx, network::LinearLayer};
 
-const L1_SIZE: usize = 2 * 512; // 2 represents (black + white)
+const L0_SIZE: usize = 2 * 512; // 2 represents (black + white)
 
 
-#[derive(Debug, Clone, Copy)]
-pub(crate) struct Accumulator {
-    pub(crate) white: [i16; L1_SIZE], // A_w
-    pub(crate) black: [i16; L1_SIZE], // A_b
+// A single AVX2 register can fit 16 i16 values, and there are 16AVX2 registers (32 since AVX512) (stockfish docs)
+
+#[repr(align(64))]
+struct Accumualator<const M: usize, N> {
+    v: [[N; M]; PLAYERS_COUNT]
 }
 
-
-impl Default for Accumulator {
-    fn default() -> Self {
-        Self { white: [0; L1_SIZE], black: [0; L1_SIZE], }
-    }
-}
-
-impl Accumulator {
-    pub(crate) fn refresh<const U: usize, const M: usize>(&self, color: Color, layer: LinearLayer<i16, U, M>, active_features: Vec<FeatureIdx>) -> Accumulator {
-        let mut new_acc = Self::default();
-
-        // First we copy the layer bias, that's out starting point
-        for i in 0..U {
-            [&mut new_acc.white, &mut new_acc.black][color][i] = layer.bias[i];
-        }
-
-        // Then we just accumulate all the columns for the active features. That's what accumulator's do
-        for a in active_features {
-            for i in 0..U {
-                [&mut new_acc.white, &mut new_acc.black][color][i] = layer.weight[*a][i];
-            }
-        }
-
-        new_acc
-    }
-
-
-    pub(crate) fn update<const U: usize, const M: usize>(&self, color: Color, layer: LinearLayer<i16, U, M>, removed_features: Vec<FeatureIdx>, added_features: Vec<FeatureIdx>) -> Self {
-        let mut new_acc = Self::default();
-
-        // First we copy the previous values, that's our starting point
-        for i in 0..M {
-            [&mut new_acc.white, &mut new_acc.black][color][i] = [&self.white, &self.white][color][i];
-        }
-
-        // Then we subtract the weights of the removed features
-        for r in removed_features {
-            for i in 0..M {
-                [&mut new_acc.white, &mut new_acc.black][color][i] -= layer.weight[*r][i];
-            }
-        }
-
-        for a in added_features {
-            for i in 0..M {
-                [&mut new_acc.white, &mut new_acc.black][color][i] += layer.weight[*a][i];
-            }
-        }
-
-        new_acc
+impl<N> Accumualator<L0_SIZE, N> {
+    pub fn refresh_accumulator<T, const U: usize, const V: usize>(
+        layer: LinearLayer<T, U, V>, 
+        new_acc: &Self,
+        active_features: &Vec<FeatureIdx>,
+        color: Color
+    ) {
+        const REGISTER_WIDTH: usize = 256/16;
+        const NUM_CHUNKS: usize = L0_SIZE / REGISTER_WIDTH;
+        let mut regs: [__m256i; NUM_CHUNKS] = unsafe { [std::mem::zeroed(); NUM_CHUNKS] };
     }
 }
