d9445906a7d0163e99283e4dafa716c3049189de
Tolumide Shopein <tolumideshopein@gmail.com>
2024-09-24 02:15:06 +0200

    Initial iteration of untested update method on the accumualtor

--- src/nnue_/accumulator.rs
+++ src/nnue_/accumulator.rs
@@ -36,11 +36,12 @@ impl<const U: usize> Default for Accumualator<Feature, U> {
 
 
 impl<const U: usize> Accumualator<Feature, U> {
+    const REGISTER_WIDTH: usize = 256/16; // 16
+
     pub(crate) unsafe fn refresh(board: &Board) -> Self {
         let mut acc = Accumualator::default();
 
-        const REGISTER_WIDTH: usize = 256/16; // 16
-        let num_chunks: usize = U / REGISTER_WIDTH; // 1024/16 = 64 (U would usually be the L1SIZE)
+        let num_chunks: usize = U / Self::REGISTER_WIDTH; // 1024/16 = 64 (U would usually be the L1SIZE)
 
         let mut active_features = Vec::with_capacity(board.get_occupancy(Both).count_ones() as usize);
         
@@ -66,7 +67,7 @@ impl<const U: usize> Accumualator<Feature, U> {
             for i in 0..num_chunks {
                 // If there is a problem here, please confirm that halfa_idx works as it should, compare directly with the one from pytorch-NNUE
                 let color_idx = (color as usize * num_chunks) + i;
-                *regs.as_mut_ptr().add(color_idx) = _mm256_load_si256(MODEL.features_bias.as_ptr().add(i * REGISTER_WIDTH) as *const __m256i);
+                *regs.as_mut_ptr().add(color_idx) = _mm256_load_si256(MODEL.features_bias.as_ptr().add(i * Self::REGISTER_WIDTH) as *const __m256i);
                 // _mm256_store_si256(acc[color].as_mut_ptr().add(i) as *mut __m256i, *(MODEL.features_bias.as_ptr().add(i * REGISTER_WIDTH) as *const __m256i));
             }
         }
@@ -78,21 +79,65 @@ impl<const U: usize> Accumualator<Feature, U> {
                 // for any value of "a", by the end of this loop, we'd have loaded all weights that belong to "a"
                 // this specifically updates all the values representing the feature "a" on the board
                 // we can only load 16 i16s at a time, weil 16*16=256 (size of an AVX2 register)
-                let idx = (*a * U) + (i*REGISTER_WIDTH);
-                let color_idx = (color as usize * num_chunks) + i;
+                let idx = (*a * U) + (i*Self::REGISTER_WIDTH);
+                let reg_idx = (color as usize * num_chunks) + i;
 
                 let weights = *(MODEL.feature_weights.as_ptr().add(idx) as *const __m256i);
                 // y = Ax + b (where A is the feature, x is the weight, and b is bias)
-                *regs.as_mut_ptr().add(color_idx) = _mm256_add_epi16(weights, *(regs.as_ptr().add(color as usize + i)));
+                *regs.as_mut_ptr().add(reg_idx) = _mm256_add_epi16(*(regs.as_ptr().add(reg_idx)), weights);
             }
         }
 
         for i in 0..num_chunks {
                 let black_idx = num_chunks + i;
-            _mm256_store_si256(acc[Color::White].as_mut_ptr().add(i*REGISTER_WIDTH) as *mut __m256i, *regs.as_ptr().add(i));
-            _mm256_store_si256(acc[Color::Black].as_mut_ptr().add(i*REGISTER_WIDTH) as *mut __m256i, *regs.as_ptr().add(black_idx));
+            _mm256_store_si256(acc[Color::White].as_mut_ptr().add(i*Self::REGISTER_WIDTH) as *mut __m256i, *regs.as_ptr().add(i));
+            _mm256_store_si256(acc[Color::Black].as_mut_ptr().add(i*Self::REGISTER_WIDTH) as *mut __m256i, *regs.as_ptr().add(black_idx));
         }
-            acc
+        
+        acc
+    }
+
+    pub(crate) unsafe fn update(&self, removed_features: &Vec<(Color, FeatureIdx)>, added_features: &Vec<(Color, FeatureIdx)>) -> Self {
+        let num_chunks: usize = U / Self::REGISTER_WIDTH; // 1024/16 = 64 (U would usually be the L1SIZE)
+
+        let mut acc = Accumualator::default();
+        let mut regs: Vec<__m256i> = Vec::with_capacity(num_chunks * PLAYERS_COUNT);
+
+        
+        // Load the previous values to registers and operate on registers only
+        for i in 0..num_chunks {
+            let black_idx = num_chunks + i;
+            *regs.as_mut_ptr().add(i) = _mm256_load_si256(self.white.as_ptr().add(i * Self::REGISTER_WIDTH));
+            *regs.as_mut_ptr().add(black_idx) = _mm256_load_si256(self.black.as_ptr().add(i * Self::REGISTER_WIDTH));
+        }
+
+        for (color, f_idx) in removed_features.iter() {
+            for i in 0..num_chunks {
+                let model_idx = (**f_idx * U) + (i * Self::REGISTER_WIDTH);
+                let regs_idx = (*color as usize * num_chunks) + i;
+
+                let weights = *(MODEL.feature_weights.as_ptr().add(model_idx) as *const __m256i);
+                *regs.as_mut_ptr().add(regs_idx) = _mm256_sub_epi16(*(regs.as_ptr().add(regs_idx)), weights);
+            }
+        }
+        
+        for (color, f_idx) in added_features.into_iter() {
+            for i in 0..num_chunks {
+                let mode_idx = (**f_idx * U) + (i * Self::REGISTER_WIDTH);
+                let regs_idx = (*color as usize * num_chunks) + i;
+
+                let weights = *(MODEL.feature_weights.as_ptr().add(mode_idx) as *const __m256i);
+                *regs.as_mut_ptr().add(regs_idx) = _mm256_add_epi16(*(regs.as_ptr().add(regs_idx)), weights);
+            }
+        }
+        
+        for i in 0..num_chunks {
+            let black_idx = num_chunks + i;
+            _mm256_store_si256(acc[Color::White].as_mut_ptr().add(i*Self::REGISTER_WIDTH) as *mut __m256i, *regs.as_ptr().add(i));
+            _mm256_store_si256(acc[Color::Black].as_mut_ptr().add(i*Self::REGISTER_WIDTH) as *mut __m256i, *regs.as_ptr().add(black_idx));
+        }
+    
+        acc
     }
 
 
