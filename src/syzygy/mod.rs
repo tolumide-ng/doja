01986744dbf8ef6d5817d1eafa528d25184845a0
Tolumide Shopein <tolumideshopein@gmail.com>
2024-10-03 02:14:40 +0200

    Initial creation of syzygy probing methods: incomplete ideation

--- src/syzygy/mod.rs
+++ src/syzygy/mod.rs
@@ -1,2 +1,36 @@
+use std::ops::Deref;
+
+use bindings::TB_LARGEST;
+
+use crate::{board::position::Position, color::Color};
+
+#[allow(dead_code, non_camel_case_types, non_upper_case_globals, non_snake_case)]
 mod bindings;
 pub(crate) mod probe;
+
+#[derive(Debug)]
+struct SyBoard(Position);
+
+
+impl Deref for SyBoard {
+    type Target = Position;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+
+impl TryFrom <Position> for SyBoard {
+    type Error = &'static str;
+
+    fn try_from(value: Position) -> Result<Self, Self::Error> {
+        // Maximum number of pieces supported for this Syzygy tablebase
+        let max_pieces = unsafe { TB_LARGEST };
+        if value.fifty.iter().sum::<u8>() == 0 && value.castling_rights.is_empty() && value.get_occupancy(Color::Both).count_ones() <= max_pieces {
+            return Ok(SyBoard(value))
+        }
+
+        Err("Board does not meet the requirements for Syzygy Tablebase probing")
+    }
+}
---
+++ src/syzygy/probe.rs
@@ -0,0 +1,41 @@
+use std::ffi::CString;
+
+use crate::board::{position::Position, state::board::Board};
+
+use super::bindings::{tb_init, TB_BLESSED_LOSS, TB_CURSED_WIN, TB_DRAW, TB_LOSS, TB_WIN};
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub(crate) enum WDL {
+    Win, Loss, Draw
+}
+
+impl TryFrom<u32> for WDL {
+    type Error = &'static str;
+
+    fn try_from(value: u32) -> Result<Self, Self::Error> {
+        match value {
+            TB_WIN => Ok(WDL::Win),
+            TB_LOSS => Ok(WDL::Loss),
+            TB_DRAW | TB_CURSED_WIN | TB_BLESSED_LOSS => Ok(WDL::Draw),
+            _ => Err("Unrecognized value for win_draw_loss"),
+        }
+    }
+}
+
+
+
+/// SyZyGy Tablase
+pub(crate) struct TableBase {
+    /// distance to zero
+    dst: usize,
+}
+
+impl TableBase {
+    pub(crate) fn init(path: &str) {
+        unsafe {
+            let p = CString::new(path).unwrap();
+            let res = tb_init(p.as_ptr());
+            assert!(res, "Could not initialize Syzygy tablebase at: {path}");
+        }
+    }
+}
