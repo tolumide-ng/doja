d858f25fe866189b722afc15cff25e14bfc67bd1
Tolumide Shopein <tolumideshopein@gmail.com>
2024-10-05 07:06:50 +0200

    Initial refactoring of Control to 'Clock'

---
+++ src/search/clock.rs
@@ -0,0 +1,12 @@
+pub(crate) struct Clock {
+    movestogo: u32,
+    movetime: u128,
+    time: u128,
+    inc: u32, 
+    startime: Instant,
+    stoptime: u128,
+    timeset: bool,
+    stopped: AtomicBool,
+    depth: u8
+
+}
