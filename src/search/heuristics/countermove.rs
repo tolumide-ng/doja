df82979edb0120357898124408153cd677d3f5b5
Tolumide Shopein <tolumideshopein@gmail.com>
2024-10-09 23:00:46 +0200

    Comments out counterMove heuristics

---
+++ src/search/heuristics/countermove.rs
@@ -0,0 +1,19 @@
+// Not implementing this for now: owing to comments from here:
+// https://talkchess.com/viewtopic.php?t=76255
+// I should probably read: https://www.researchgate.net/publication/306413279_The_Countermove_Heuristic
+
+// use crate::{bit_move::Move, constants::TOTAL_SQUARES, squares::Square};
+
+// /// CounterMove Heuristic
+// /// https://www.chessprogramming.org/Countermove_Heuristic
+// pub(crate) struct CounterMove([u16; TOTAL_SQUARES * TOTAL_SQUARES]);
+
+// impl CounterMove {
+//     pub(crate) fn new() -> Self {
+//         CounterMove([0; TOTAL_SQUARES * TOTAL_SQUARES])
+//     }
+
+//     pub(crate) fn store(&mut self, src: Square, tgt: Square, mv: &Move) {
+//         self.0[(src as usize * TOTAL_SQUARES) + tgt as usize] = **mv;
+//     }
+// }
--- src/search/heuristics/mod.rs
+++ src/search/heuristics/mod.rs
@@ -1,3 +1,4 @@
 pub(crate) mod pv_table;
 pub(crate) mod killer_moves;
 pub(crate) mod history;
+pub(crate) mod countermove;
