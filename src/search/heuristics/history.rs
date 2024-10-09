d298a2f4b14184c5fb3476a6576d5649843b5c03
Tolumide Shopein <tolumideshopein@gmail.com>
2024-10-09 21:50:09 +0200

    Modifies the history_heuristics table

--- src/search/heuristics/history.rs
+++ src/search/heuristics/history.rs
@@ -1,15 +1,17 @@
 use crate::{board::piece::Piece, constants::{TOTAL_PIECES, TOTAL_SQUARES}, squares::Square};
 
+
+// [i16; TOTAL_SQUARES]; TOTAL_PIECES]
 /// History Heuristics
 #[derive(Debug)]
-pub(crate) struct HistoryHeuristic([[i16; TOTAL_SQUARES]; TOTAL_PIECES]);
+pub(crate) struct HistoryHeuristic([i16; TOTAL_SQUARES * TOTAL_PIECES]);
 
 
 impl HistoryHeuristic {
     const MAX_HISTORY: i16 = i16::MAX;
 
     pub(crate) fn new() -> Self {
-        Self ([[0; TOTAL_SQUARES]; TOTAL_PIECES])
+        Self ([0; TOTAL_SQUARES * TOTAL_PIECES])
     }
 
     const fn bonus(depth: u8) -> i16 {
@@ -21,10 +23,11 @@ impl HistoryHeuristic {
     pub(crate) fn update(&mut self, piece: Piece, sq: Square, depth: u8) {
         let bonus = Self::bonus(depth);
         let clamped_bonus = i16::clamp(bonus, i16::MIN, i16::MAX);
-        self.0[piece][sq as usize] += clamped_bonus.saturating_sub(self.0[piece][sq]) * clamped_bonus.abs() / Self::MAX_HISTORY;
+        let history = unsafe { self.0.get_unchecked_mut((piece as usize * TOTAL_PIECES) + sq as usize) };
+        *history += *history * clamped_bonus.abs() / Self::MAX_HISTORY;
     }
     
     pub(crate) fn get(&self, piece: Piece, sq: Square) -> i16 {
-        self.0[piece][sq]
+        *unsafe { self.0.get_unchecked((piece as usize * TOTAL_PIECES) + sq as usize) }
     }
 }
